c6c1de49553892b94c57463873abff2ea9a39d03
xFellern <fellernik2006@gmail.com>
2025-10-28 18:44:04 +0100

    Add files via upload

---
+++ VS Project/MJLFO_v2_0/MJLFO_v2_0/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Minecraft Java Log File Organizer")]
+[assembly: AssemblyDescription("Organize your .log files from Minecraft: Java Edition")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xFellern")]
+[assembly: AssemblyProduct("Minecraft Java Log File Organizer")]
+[assembly: AssemblyCopyright("© 2024 - 2025 by xFellern | github.com/xFellern")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("c825c629-6240-468a-9eed-12a7e28972e8")]
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("")]
